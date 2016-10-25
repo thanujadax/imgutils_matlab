54ee7034ad0851838b55a8a022893d5aa3df487e
Thanuja Ambegoda <thanujadax@gmail.com>
2016-10-25 21:45:37 +0200

    added multiple membrane thinning iterations

--- growCCdir.m
+++ growCCdir.m
@@ -1,17 +1,20 @@
-function growCCdir(inputDir,outputDir,inds,fmt)
+function growCCdir(inputDir,outputDir,inds,fmt,px)
 % for all rgb images in the inputDir, grow their connected components by 1 pix
 % in a 4nh and write back to outputDir.
 % if inds==1, the output will contain index images. Else it will be binary
+% px - number of times (=pixels) the CCs have to be grown
 
 inputFileList = dir(fullfile(inputDir,strcat('*.',fmt)));
 
 for i=1:length(inputFileList)
     imIn = double(imread(fullfile(inputDir,inputFileList(i).name)));
-    imIn = rgb2gray(imIn);
-    imIn = im2bw(imIn,0);
-    imOut = growCCs(imIn,inds);
+    imIn = rgb2gray(imIn);   
+    for j=1:px
+        imIn = im2bw(imIn,0);
+        imIn = growCCs(imIn,inds);
+    end
     outFileName = fullfile(outputDir,inputFileList(i).name);
-    imwrite(imOut,outFileName);
+    imwrite(imIn,outFileName);
     
 end
 
--- growCCs.m
+++ growCCs.m
@@ -9,8 +9,7 @@ cc = bwconncomp(imIn);
 [sizeR,sizeC] = size(imIn);
 imOut = zeros(sizeR,sizeC);
 
-
-if(inds)
+if(inds>0)
     for i=1:length(cc.PixelIdxList)
         % for each cc get the expanded 4nh pixel set and set a unique ID i
         nh4pixels = get4nhpixels(cc.PixelIdxList{i},sizeR,sizeC);
@@ -23,3 +22,5 @@ else
         imOut(nh4pixels) = 1;
     end
 end
+% normalize imOut
+imOut = imOut./(max(max(imOut)));
